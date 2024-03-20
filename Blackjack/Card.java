f22c3c445fa45702f8565258af9aff62791adf7f
belalE <belalelsiesy@gmail.com>
2024-03-19 20:11:27 -0500

    add hand class
    add hand class, updated deck and card, error with calculating hand value

--- Blackjack/Card.java
+++ Blackjack/Card.java
@@ -10,27 +10,53 @@ public class Card extends Actor
 {
     int value;
     String imgTitle;
+    String faceValue;
+    boolean visible;
     
-    Card(int cardNum) {
+    Card(int cardNum, boolean visible) {
+        visible = visible;
         value = cardNum % 13;  
         if (value == 1) {
             imgTitle = "CardAce.png";
+            faceValue = "A";
         }
         else if (value == 11) {
             imgTitle = "CardJack.png";
+            faceValue = "J";
         }
         else if (value == 12) {
             imgTitle = "CardQueen.png";
+            faceValue = "Q";
         }
         else if (value == 0) {
             imgTitle = "CardKing.png";
+            faceValue = "K";
         }
         else {
             imgTitle = "Card" + value + ".png";
+            faceValue = Integer.toString(value);
         }
+        if (visible) {
+            this.setImage(imgTitle);
+        } else {
+            this.setImage("CardBack.png");
+        }
+    }
+    
+    public String getValue() {
+        return faceValue;
+    }
+    
+    public boolean isVisible() {
+        return visible;
+    }
+    
+    public void showCard() {
+        visible = true;
         this.setImage(imgTitle);
     }
     
+
     /**
      * Act - do whatever the Card wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
--- Blackjack/Table.java
+++ Blackjack/Table.java
@@ -8,11 +8,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Table extends World
 {
-
+    Deck deck;
+    Hand dealerHand;
+    Hand playerHand;
     /**
      * Constructor for objects of class MyWorld.
      * 
      */
+
     public Table()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
@@ -21,5 +24,26 @@ public class Table extends World
         background.setColor(new Color(0, 120, 40));
         background.fill();
         setBackground(background);
+        
+        // Testing for deck
+        deck = new Deck();
+        Card card1 = deck.draw(true);
+        Card card2 = deck.draw(false);
+        dealerHand = new Hand(80, 100);
+        dealerHand.addCard(card1);
+        dealerHand.addCard(card2);
+        addObject(dealerHand, 0,0);
+        dealerHand.renderHand();
+        System.out.println(dealerHand.getValue());
+        
+        Card card3 = deck.draw(true);
+        Card card4 = deck.draw(true);
+        playerHand = new Hand(80,300);
+        playerHand.addCard(card3);
+        playerHand.addCard(card4);
+        addObject(playerHand, 0,0);
+        playerHand.renderHand();
+        System.out.println(playerHand.getValue());        
+        
     }
 }
