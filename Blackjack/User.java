d3ddb22eae8015b4c14c1ce399eab02066e1b87a
Anish Sivakumar <73561392+anish-siv@users.noreply.github.com>
2024-03-20 22:59:24 -0500

    Update User.java

--- Blackjack/User.java
+++ Blackjack/User.java
@@ -1,28 +1,35 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class User here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class User extends Player
-{
-    User(Deck deck){
+public class User extends Player {
+    User(Deck deck) {
         super(80, 300, deck);
     }
     
-    void setup(){
+    void setup() {
         super.setup();
         draw(true);
+    }
+    
+    // Method to handle when the user hits
+    public void hit() {
+        if (!isTurnOver()) {
+            draw(true); // Draw a card and show it
+            if (isTurnOver()) {
+                endTurn(); // End the user's turn if they bust or reach 21
+            }
+        }
+    }
+    
+    // Method to handle when the user stands
+    public void stand() {
+        if (!isTurnOver()) {
+            endTurn(); // End the user's turn
+        }
     }
     
     /**
      * Act - do whatever the User wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act()
-    {
-        // Add your action code here.
+    public void act() {
+        // Add your action code here, if any
     }
 }
