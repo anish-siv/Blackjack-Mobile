5ba7ad9e6a888a00b9fd83d7e4b0485d4d0663a4
Anish Sivakumar <73561392+anish-siv@users.noreply.github.com>
2024-03-20 22:58:49 -0500

    Update Stand.java

--- Blackjack/Stand.java
+++ Blackjack/Stand.java
@@ -6,29 +6,11 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Stand extends Button
-{
-    /**
-     * Act - do whatever the Stand wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act()
-    {
-        // Add your action code here.
-        Table world = (Table) getWorld();
-        
-        if ( Greenfoot.mouseClicked(this) && !world.isGameOver() )
-        {
-            stand();
+public class Stand extends Button {
+    public void act() {
+        if (Greenfoot.mouseClicked(this)) {
+            Table table = (Table) getWorld();
+            table.userStands(); // Call the userStands method in Table class
         }
-    }
-    
-    /**
-     * Stand at the current score, when the player hits the stand button.
-     */
-    private void stand()
-    {
-        Table world = (Table) getWorld();
-        world.switchTurn();
     }
 }
